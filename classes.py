1d8ec983646a2871a9dd30a89722c031fde3cbd1
gigilka <kuchin-02@mail.ru>
2023-02-07 15:12:26 +0300

    based window

---
+++ classes.py
@@ -0,0 +1,16 @@
+import tkinter
+import customtkinter
+class App(customtkinter.CTk):
+    def __init__(self):
+        super().__init__()
+        self.geometry("600x500")
+        self.title("Student Calculator")
+        self.iconbitmap('main.ico')
+
+        # add widgets to app
+        self.button = customtkinter.CTkButton(self, command=self.button_click)
+        self.button.grid(row=0, column=0, padx=20, pady=10)
+
+    # add methods to app
+    def button_click(self):
+        print("button click")
---
+++ main.py
@@ -0,0 +1,10 @@
+import tkinter
+import customtkinter
+from classes import *
+customtkinter.set_appearance_mode("light")  # Modes: system (default), light, dark
+customtkinter.set_default_color_theme("blue")  # Themes: blue (default), dark-blue, green
+
+
+        
+app = App()  # create CTk window like you do with the Tk window
+app.mainloop()
