80e57a698fe9dfbf1cf5dec5c41f2636540d4cd4
gigilka <kuchin-02@mail.ru>
2023-02-07 18:36:34 +0300

    1

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
 import tkinter
 import customtkinter
 from classes import *
-customtkinter.set_appearance_mode("light")  # Modes: system (default), light, dark
-customtkinter.set_default_color_theme("blue")  # Themes: blue (default), dark-blue, green
+# Modes: system (default), light, dark
+customtkinter.set_appearance_mode("light")
+# Themes: blue (default), dark-blue, green
+customtkinter.set_default_color_theme("blue")
 
 
-        
 app = App()  # create CTk window like you do with the Tk window
 app.mainloop()
